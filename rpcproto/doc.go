1cf58996b15eddfb4cf298ca191270b9b0d48e7e
Donovan Solms <donovan.solms@gmail.com>
2019-01-22 22:58:28 +0200

    Added license notice

--- rpcproto/doc.go
+++ rpcproto/doc.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2018 Donovan Solms / MiningHQ. Licensed under the MIT license.
+// See LICENSE in the root of this repository for details.
+
 // Package rpcproto contains protocol buffer definitions for
 // Miner <-> MiningHQ RPC communication
 package rpcproto
